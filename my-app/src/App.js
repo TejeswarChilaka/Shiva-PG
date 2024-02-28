97e04b5d4ee8472da2f8ecc3e33ff06859c9e3eb
TejeswarChilaka <chilaka.tejeswar@gmail.com>
2024-02-28 07:31:13 +0530

    Created Links to all the pages along with NavBar.

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,27 @@
-import 
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Rooms from "./pages/Rooms";
+import Dining from "./pages/Dining";
+import Gym from "./pages/Gym";
+import Maintenance from "./pages/Maintenance";
+import NavBar from "./NavBar";
 import './App.css';
 
 function App() {
   return (
     <>
-    <p></p>
-    
+      <BrowserRouter>
+        <NavBar></NavBar>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/about" element={<About />}></Route>
+          <Route path="/rooms" element={<Rooms />}></Route>
+          <Route path="/dining" element={<Dining />}></Route>
+          <Route path="/gym" element={<Gym />}></Route>
+          <Route path="/maintenance" element={<Maintenance />}></Route>
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
